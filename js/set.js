202e9e518c9099c3acce3b84896b5aec29c41f92
CraftYouMe <651446044@qq.com>
2025-05-26 20:01:51 +0800

    修复搜索引擎不安全问题

--- js/set.js
+++ js/set.js
@@ -26,7 +26,7 @@ var se_list_preinstall = {
     '2': {
         id: 2,
         title: "必应",
-        url: "http://cn.bing.com/search?q=%s&go=&form=QBLH&qs=n&sk=",
+        url: "https://cn.bing.com/search?q=%s&go=&form=QBLH&qs=n&sk=",
         name: "q",
         icon: "iconfont icon-bing",
     },
