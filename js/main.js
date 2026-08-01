c0a15e7eb928f1eeac8ec19ab7f7a1e9c043b49d
CraftYouMe <651446044@qq.com>
2026-08-01 21:21:03 +0800

    统一导航与设置中心玻璃视觉
    统一收藏 favicon 容器，微调搜索框玻璃层次和卡片 hover 反馈，并让设置中心尺寸、顶栏与选项卡片沿用收藏中心视觉语言。

--- js/main.js
+++ js/main.js
@@ -164,7 +164,7 @@ var navSitesLoadPromise = null;
 var navStatusLoadPromise = null;
 var settingsLoadPromise = null;
 var wallpaperSettingsLoadPromise = null;
-var sksirAssetBase = "https://yuanone-blog-picture.oss-cn-beijing.aliyuncs.com/sksir/2026.08.01.8/";
+var sksirAssetBase = "https://yuanone-blog-picture.oss-cn-beijing.aliyuncs.com/sksir/2026.08.01.9/";
 
 function getVersionedAssetUrl(src) {
     if (!src || !/^(?:\.\/|\/(?!\/))/.test(src)) return src;
